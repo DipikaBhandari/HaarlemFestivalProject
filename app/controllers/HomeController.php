63c915288e1ae7b16248306f4e50cf5bf6358910
DipikaBhandari <bdipika076@gmail.com>
2024-03-01 11:52:35 +0100

    ignore

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,15 +4,5 @@ namespace App\Controllers;
 
 class HomeController
 {
-    private $homeService;
 
-    public function __construct()
-    {
-        $this->homeService = new \App\Service\homeService();
-    }
-
-    public function index()
-    {
-        require __DIR__ . '/../views/festival/index.php';
-    }
 }
