35ba2eaa764fd173de56c721e3f274e9800a9abb
pierickm <113185406+pierickm@users.noreply.github.com>
2024-03-17 17:29:30 +0100

    make hardcoded pageId unneccessary in homecontroller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,14 +13,17 @@ class HomeController
 
     public function index()
     {
-        $sections = $this->homeService->getSectionsByPage(1);
+        if(isset($_GET['pageId'])) {
+            $pageId = filter_var($_GET['pageId'], FILTER_SANITIZE_NUMBER_INT);
+            $sections = $this->homeService->getSectionsByPage($pageId);
 
-        foreach ($sections as $key => $section) {
-            if ($section['type'] === 'crossnavigation') {
-                $sections[$key]['carouselItems'] = $this->homeService->getCarouselItemsBySection($section['sectionId']);
+            foreach ($sections as $key => $section) {
+                if ($section['type'] === 'crossnavigation') {
+                    $sections[$key]['carouselItems'] = $this->homeService->getCarouselItemsBySection($section['sectionId']);
+                }
+                $sections[$key]['images'] = $this->homeService->getImageBySection($section['sectionId']);
+                $sections[$key]['paragraphs'] = $this->homeService->getParagraphsBySection($section['sectionId']);
             }
-            $sections[$key]['images'] = $this->homeService->getImageBySection($section['sectionId']);
-            $sections[$key]['paragraphs'] = $this->homeService->getParagraphsBySection($section['sectionId']);
         }
 
         require __DIR__ . '/../views/home/index.php';
