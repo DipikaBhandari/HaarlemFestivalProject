f74495b8813699bf6d0c07038823a0f6ce504d55
pierickm <113185406+pierickm@users.noreply.github.com>
2024-04-05 16:16:24 +0200

    add error handling to homepage files

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,15 +14,20 @@ class HomeController
     public function index()
     {
         $pageId = HOME_PAGE_ID;
-        $sections = $this->homeService->getSectionsByPage($pageId);
+        try{
+            $sections = $this->homeService->getSectionsByPage($pageId);
 
-        foreach ($sections as $key => $section) {
-            if ($section['type'] === 'crossnavigation') {
-                $sections[$key]['carouselItems'] = $this->homeService->getCarouselItemsBySection($section['sectionId']);
+            foreach ($sections as $key => $section) {
+                if ($section['type'] === 'crossnavigation') {
+                    $sections[$key]['carouselItems'] = $this->homeService->getCarouselItemsBySection($section['sectionId']);
+                }
+                $sections[$key]['images'] = $this->homeService->getImageBySection($section['sectionId']);
+                $sections[$key]['paragraphs'] = $this->homeService->getParagraphsBySection($section['sectionId']);
             }
-            $sections[$key]['images'] = $this->homeService->getImageBySection($section['sectionId']);
-            $sections[$key]['paragraphs'] = $this->homeService->getParagraphsBySection($section['sectionId']);
+            require __DIR__ . '/../views/home/index.php';
+        } catch (\Exception $e) {
+            error_log('Error retrieving the page content: ' . $e->getMessage());
+            echo 'Error retrieving the page content. Please try again later.';
         }
-        require __DIR__ . '/../views/home/index.php';
     }
 }
